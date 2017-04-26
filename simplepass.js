46a6a0df55069d5618567020aa107ff546d3ecf4
HarryWu00 <8hwu@stpatrick.org>
2017-04-26 11:24:34 -0500

    Day 1

---
+++ simplepass.js
@@ -0,0 +1,24 @@
+//type checkString below
+function checkString(str){
+    if(str.length>=8){
+        return str;
+    }
+    else if (str.length>=20){
+        return str;
+    }
+    else{
+        throw new Error("This password is in incorrect length")
+    }
+}
+
+
+//type getString below
+function getString(str){
+    try{
+        str = checkString(str);
+        catch (e){e}{
+            console.log(e.message);
+            getString(str);
+        }
+    }
+}
